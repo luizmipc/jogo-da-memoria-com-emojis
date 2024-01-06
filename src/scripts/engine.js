18e918a1a1c9b97cd726eebec0d1eb9c253d5d79
Luiz Marcelo <99672783+luizmipc@users.noreply.github.com>
2024-01-06 20:08:20 -0300

    criando efeito de rotate nos cards

---
+++ src/scripts/engine.js
@@ -0,0 +1,21 @@
+const emojis = [
+    "😺", "😺",
+    "🦝", "🦝",
+    "🦊", "🦊",
+    "🐶", "🐶",
+    "🐵", "🐵",
+    "🦁", "🦁",
+    "🐯", "🐯",
+    "🐮", "🐮"
+];
+let openCards = [];
+
+let shuffleEmojis = emojis.sort(()=>(Math.random() > 0.5 ? 2: -1));
+
+for(let i=0; i < emojis.length; i++)
+{
+    let box = document.createElement("div");
+    box.className = "item";
+    box.innerHTML = shuffleEmojis[i];
+    document.querySelector(".game").appendChild(box);
+}
