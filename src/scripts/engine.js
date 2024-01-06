0f69239aa966203ba9756c1c7594a9513d3d8be3
Luiz Marcelo <99672783+luizmipc@users.noreply.github.com>
2024-01-06 20:53:36 -0300

    jogo completo

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -34,6 +34,24 @@ function handleClick() {
     console.log(openCards);
 }
 
-function checkMatch() {
+function playSound(audioName){
+    let audio = new Audio(`./src/audios/${audioName}.m4a`);
+    audio.volume = 0.1;
+    audio.play();
+}
 
+function checkMatch() {
+    if(openCards[0].innerHTML === openCards[1].innerHTML) {
+        openCards[0].classList.add("boxMatch");
+        openCards[1].classList.add("boxMatch");
+    } else {
+        openCards[0].classList.remove("boxOpen");
+        openCards[1].classList.remove("boxOpen");
+    }
+
+    openCards = [];
+    if (document.querySelectorAll(".boxMatch").length === emojis.length) {
+        playSound("win");
+        alert("Você venceu!");
+    }
 }
