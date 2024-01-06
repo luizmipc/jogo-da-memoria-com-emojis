90b04d2210b311fef390716ab0a532414e650998
Luiz Marcelo <99672783+luizmipc@users.noreply.github.com>
2024-01-06 20:39:39 -0300

    criando a condição de vitória

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -17,5 +17,23 @@ for(let i=0; i < emojis.length; i++)
     let box = document.createElement("div");
     box.className = "item";
     box.innerHTML = shuffleEmojis[i];
+    box.onclick = handleClick;
     document.querySelector(".game").appendChild(box);
+}
+
+function handleClick() {
+    if (openCards.length < 2) {
+        this.classList.add("boxOpen");
+        openCards.push(this);
+    }
+
+    if(openCards.length == 2) {
+        setTimeout(checkMatch, 500);
+    }
+
+    console.log(openCards);
+}
+
+function checkMatch() {
+
 }
